a5dd71cf4cdb7c06e9aca91f2cee2e6a8de8040b
johanssonida1996 <70290036+johanssonida1996@users.noreply.github.com>
2020-11-04 10:11:23 +0100

    Add files via upload

---
+++ SQLQuery1____.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Customer(
+	SSNumber int not null primary key,
+	[Name] nvarchar(50) not null,
+	PhoneNumber int not null,
+	Email nvarchar(50) not null
+)
+GO
+
+CREATE TABLE SupportCases(	
+	CaseNumber int identity(1,1) primary key,
+	CustomerNumber int not null references Customer(SSNumber),
+	[Status] nvarchar(50) not null,
+	[Description] nvarchar(500) not null,	
+	Title nvarchar(50) not null,
+	Category nvarchar(50) not null
+	[Time] DateTime  
+)
